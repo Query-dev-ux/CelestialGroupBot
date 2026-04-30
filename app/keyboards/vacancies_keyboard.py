61f2677245c8e76903d1bd273be0bcbe1844fabe
Query-dev-ux <query.dclxv1@mail.ru>
2026-04-30 20:07:09 +0300

    Добавление дополнительных вакансий

--- app/keyboards/vacancies_keyboard.py
+++ app/keyboards/vacancies_keyboard.py
@@ -8,7 +8,7 @@ def vacancies_keyboard():
         [InlineKeyboardButton(text="Media Buyer (Google)", callback_data="vac_google")],
         [InlineKeyboardButton(text="SEO Specialist", callback_data="vac_seo")],
         [InlineKeyboardButton(text="HR Assistant", callback_data="vac_hr")],
-        [InlineKeyboardButton(text="HR Business Development Manager (iGaming)", callback_data="vac_bizdev")],
+        [InlineKeyboardButton(text="Business Development Manager (iGaming)", callback_data="vac_bizdev")],
         [InlineKeyboardButton(text="Affiliate Manager (iGaming)", callback_data="vac_affiliate")],
         [InlineKeyboardButton(text="Отправить резюме", callback_data="send_cv")],
         [InlineKeyboardButton(text="⬅️ Главное меню", callback_data="back_main_menu")],
